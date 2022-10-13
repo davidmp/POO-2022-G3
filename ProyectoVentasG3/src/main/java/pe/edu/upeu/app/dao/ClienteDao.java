8dc7a805a78c75ec56eecb65953bcf2be01631a0
davidmp <mamanipari@gmail.com>
2022-10-13 16:12:48 -0500

    coneccion cliente DAO

--- ProyectoVentasG3/src/main/java/pe/edu/upeu/app/dao/ClienteDao.java
+++ ProyectoVentasG3/src/main/java/pe/edu/upeu/app/dao/ClienteDao.java
@@ -7,9 +7,12 @@ package pe.edu.upeu.app.dao;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Vector;
+import java.util.logging.Level;
 import pe.edu.upeu.app.dao.conx.Conn;
 import pe.edu.upeu.app.modelo.ClienteTO;
 import pe.edu.upeu.app.util.ErrorLogger;
@@ -35,32 +38,114 @@ public class ClienteDao implements ClienteDaoI {
 
     @Override
     public int create(ClienteTO d) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        int rsId = 0;
+        String[] returns = {"dniruc"};
+        String sql = "INSERT INTO cliente(dniruc, nombrers, tipo) "
+                + "VALUES(?,?,?)";
+        int i = 0;
+        try {
+            ps = connection.prepareStatement(sql, returns);
+            ps.setString(++i, d.getDniruc());
+            ps.setString(++i, d.getNombrers());
+            ps.setString(++i, d.getTipo());
+            rsId = ps.executeUpdate();// 0 no o 1 si commit
+            try ( ResultSet rs = ps.getGeneratedKeys()) {
+                if (rs.next()) {
+                    rsId = rs.getInt(1);
+                }
+                rs.close();
+            }
+        } catch (SQLException ex) {
+            //System.err.println("create:" + ex.toString());
+            log.log(Level.SEVERE, "create", ex);
+        }
+        return rsId;
     }
 
     @Override
     public int update(ClienteTO d) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        System.out.println("actualizar d.getDniruc: " + d.getDniruc());
+        int comit = 0;
+        String sql = "UPDATE cliente SET "
+                + "nombrers=?, "
+                + "tipo=? "
+                + "WHERE dniruc=?";
+        int i = 0;
+        try {
+            ps = connection.prepareStatement(sql);
+            ps.setString(++i, d.getNombrers());
+            ps.setString(++i, d.getTipo());
+            ps.setString(++i, d.getDniruc());
+            comit = ps.executeUpdate();
+        } catch (SQLException ex) {
+            log.log(Level.SEVERE, "update", ex);
+        }
+        return comit;
     }
 
     @Override
     public int delete(String id) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        int comit = 0;
+        String sql = "DELETE FROM cliente WHERE dniruc = ?";
+        try {
+            ps = connection.prepareStatement(sql);
+            ps.setString(1, id);
+            comit = ps.executeUpdate();
+        } catch (SQLException ex) {
+            log.log(Level.SEVERE, "delete", ex);
+            // System.err.println("NO del " + ex.toString());
+            throw new Exception("Detalle:" + ex.getMessage());
+        }
+        return comit;
     }
 
     @Override
     public List<ClienteTO> listCmb(String filter) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        List<ClienteTO> ls = new ArrayList();
+        ls.add(new ClienteTO());
+        ls.addAll(listarClientes());
+        return ls;
     }
 
     @Override
     public List listarClientes() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        List<ClienteTO> listarclientes = new ArrayList();
+        String sql = "SELECT * FROM cliente";
+        try {
+            connection = new Conn().connectSQLite();
+            ps = connection.prepareStatement(sql);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                ClienteTO cli = new ClienteTO();
+                cli.setDniruc(rs.getString("dniruc"));
+                cli.setNombrers(rs.getString("nombrers"));
+                cli.setTipo(rs.getString("tipo"));
+                listarclientes.add(cli);
+            }
+        } catch (SQLException e) {
+            System.out.println(e.toString());
+        }
+        return listarclientes;
     }
 
     @Override
     public ClienteTO buscarClientes(String dni) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        ClienteTO cliente = new ClienteTO();
+        String sql = "SELECT * FROM cliente WHERE dniruc = ?";
+        try {
+            connection = new Conn().connectSQLite();
+            ps = connection.prepareStatement(sql);
+            ps.setString(1, dni);
+            rs = ps.executeQuery();
+            if (rs.next()) {
+                cliente.setDniruc(rs.getString("dniruc"));
+                cliente.setNombrers(rs.getString("nombrers"));
+                cliente.setTipo(rs.getString("tipo"));
+            }
+        } catch (SQLException e) {
+            System.out.println(e.toString());
+        }
+        return cliente;
     }
 
     @Override
