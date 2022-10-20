b2da27fa391152bbcb93c67d9c87adf06f206b3d
AngelDX <angel.condori@gmail.com>
2022-10-20 15:03:36 -0500

    arreglo scroll

--- ProyectoVentasG3/src/main/java/pe/edu/upeu/app/gui/GUIMain.java
+++ ProyectoVentasG3/src/main/java/pe/edu/upeu/app/gui/GUIMain.java
@@ -35,14 +35,15 @@ public class GUIMain extends JFrame {
         menuBar = new JMenuBar();
         menu1 = new JMenu("Archivo");
         jmI1 = new JMenuItem("Abrir");
-        menuBar.add(menu1);
+        
         jmI2 = new JMenuItem("Adm. Cliente");
         menu1.add(jmI1);
         menu1.add(jmI2);
+        menuBar.add(menu1);
         menu1 = new JMenu("Ver");
         menuBar.add(menu1);
-        this.add(menuBar);
-
+        //this.add(menuBar);
+        
         //Accion de abbrir JTabbedPane
         MenuItemListener menuItemListener = new MenuItemListener();
         jmI1.addActionListener(menuItemListener);//accion
@@ -59,9 +60,10 @@ public class GUIMain extends JFrame {
         @Override
         public void actionPerformed(ActionEvent e) {
             System.out.println("pasa por aqui");
+             jtpane.removeAll();
             Container contai = GUIMain.this.getContentPane();
             if (e.getSource() == jmI1) {
-                jtpane.removeAll();
+               
                 jp = new JPanel();
                 jtpane.add("Prueba", jp);
                 jp = new JPanel();
@@ -87,9 +89,13 @@ public class GUIMain extends JFrame {
             }
 
             if (e.getSource() == jmI2) {
-                jtpane.removeAll();                
+                System.out.println("verrr");
                 MainCliente mc = new MainCliente();
-                jtpane.add("Cliente", mc);
+                scrollPane = new JScrollPane(mc);
+                scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+                scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+                jtpane.add("Adm. Cliente", scrollPane);
+                
                 contai.add(BorderLayout.CENTER, jtpane);
                 contai.invalidate();
                 contai.validate();
